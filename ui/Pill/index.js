c4f50909a75d5193334f9e2215fc7bbad0ef2fb5
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-06-21 13:32:44 +0100

    styled Pill and added badge (dud)

--- ui/Pill/index.js
+++ ui/Pill/index.js
@@ -1,19 +1,73 @@
-import { LitElement, html } from "lit";
+import { LitElement, html, css } from "lit";
+import { rootStyles } from "../root.styles";
 
 export class Pill extends LitElement {
 
     static properties = {
         name: {type: String},
+        color: {type: String},
     }
+
+    constructor(){
+        super();
+        this.name;
+        this.color = 'amethyst';
+    }
+
+    static styles = css`
+        ${rootStyles}
+
+        .pill::before {
+            content: "6+";
+            display: flex;
+            width: 1rem;
+            height: 1rem;
+            border-radius: 50%;
+            background-color: rgba(var(--ruby-500), 1);
+            position: absolute;
+            color: white;
+            justify-content: center;
+            align-items: center;
+            right: -0.4rem;
+            top: -0.4rem;
+        }
+
+        .pill {
+            display: flex; 
+            padding: 0.25rem 0.5rem;
+            transition-duration: 300ms; 
+            font-weight: 700; 
+            align-items: center; 
+            user-select: none; 
+            font-size: var(--text-size);
+            background-color: rgba(var(--amethyst-500) , 0.2);
+            border-radius: var(--rounded-xs);
+            color: rgba(var(--amethyst-500), 1);
+            width: fit-content;
+            text-transform: uppercase;
+            letter-spacing: 0.05rem;
+          }
+
+          :hover {
+            color: rgba(var(--amethyst-500), .2);
+          }
+            
+          .dot {
+            width: 4px;
+            height: 4px;
+            border-radius: 100%;
+            margin-right: 4px;
+            background-color: rgba(var(--amethyst-500) , 0.2);
+          }
+        `
 
     render(){
         return html`   
-        <div>
-            <div class="flex items-center">
-                <span class="h-1 w-1 mr-1 rounded-full bg-green-500"></span>
+           <div style="display:inline-block; position:relative;">
+            <div class='pill'>
                 <span>${this.name}</span>
             </div>
-        </div>
+           </div>
         `;
     }
 }
